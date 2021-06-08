7e4356f0f1c618d3f813cf5b66f8e89f3b6a9244
sk-ismail <kingofdemons077@gmail.com>
2021-06-08 23:27:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Questions from './Questions'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < Questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div>
+		<div className='app'>
+			{showScore ? (
+				
+				<div className='score-section'>
+					
+          <div class='container'>
+  <div class='hoverme'>
+    <h1>
+    You scored {score} out of {Questions.length}
+    </h1>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+    <i></i>
+  </div>
+</div>
+				</div>):(
+					
+		<div>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{Questions.length}
+						</div>
+						<div className='question-text'>{Questions[currentQuestion].questionText}</div>
+					</div>
+
+
+					<div className='answer-section'>
+					
+						{Questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{<img src={answerOption.answerText} style={{width:"100px", height:"100px"}} alt='logo'/> }</button>
+						))}
+					</div>
+					</div>
+				
+				)}
+		</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Questions.js
@@ -0,0 +1,77 @@
+import one from './images/1.png';
+import two from './images/2.png';
+import three from './images/3.png';
+import four from './images/4.png';
+import five from './images/5.png';
+import six from './images/6.png';
+import seven from './images/7.png';
+import eight from './images/8.png';
+import nine from './images/9.png';
+import ten from './images/10.png';
+import elevn from './images/11.png';
+import tweleve from './images/12.png';
+import thirteen from './images/13.png';
+import fourteen from './images/14.png';
+
+
+
+const Questions = [
+    {
+        questionText: 'Which is taller?',
+        answerOptions: [
+            { answerText: one, isCorrect: true },
+            { answerText: two, isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'Who car is faster?',
+        answerOptions: [
+            { answerText: three, isCorrect: false },
+            { answerText: four, isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'In which you will find more sugar?',       
+        answerOptions: [
+            { answerText: five, isCorrect: true },
+            { answerText: six, isCorrect: false },
+            
+        ],
+    },
+    {
+        questionText: 'Which nutrient build muscles?',
+        answerOptions: [
+            { answerText: seven, isCorrect: false },
+            { answerText: eight, isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'Enema, will help you to lose what?',
+        answerOptions: [
+            { answerText: nine, isCorrect: false },
+            { answerText: ten, isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'Untreated high blood pressure, will damage which organ?',
+        answerOptions: [
+            { answerText: elevn, isCorrect: false },
+            { answerText: tweleve, isCorrect: true },
+            
+        ],
+    },
+    {
+        questionText: 'Who is cuter?',
+        answerOptions: [
+            { answerText: thirteen, isCorrect: true },
+            { answerText: fourteen, isCorrect: false },
+            
+        ],
+    },
+];
+
+export default Questions;
